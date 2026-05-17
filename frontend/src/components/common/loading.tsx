84403e8f3ce65f8434088b120d0aa96a224c870b
Ivan <nexacoders2@gmail.com>
2026-05-17 18:37:12 +0530

    Create common/loading.tsx

---
+++ frontend/src/components/common/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center py-20">
+
+      <div className="w-14 h-14 border-4 border-cyan-500 border-t-transparent rounded-full animate-spin" />
+
+    </div>
+  );
+}
