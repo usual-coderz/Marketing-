8bab2a4d2a239289672f80a5392cdd1bdae77df0
Ivan <nexacoders2@gmail.com>
2026-05-17 18:42:19 +0530

    Create axios.ts

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:8000/api",
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
