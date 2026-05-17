9d57826db1eeb260e4c6aa63fce1167356349744
Ivan <nexacoders2@gmail.com>
2026-05-17 18:43:41 +0530

    Create constants.ts

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,15 @@
+export const MARKETPLACE_NAME = "TGMarket";
+
+export const CATEGORIES = [
+  "Telegram Channels",
+  "Telegram Groups",
+  "Telegram Bots",
+  "Premium Usernames",
+  "Telegram Stars",
+  "NFT Assets",
+];
+
+export const THEME = {
+  PRIMARY: "#22d3ee",
+  BACKGROUND: "#070B14",
+};
